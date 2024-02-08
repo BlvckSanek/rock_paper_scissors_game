854736a77e505bcae4c3a2c89b96d39d9fd140b4
Emmanuel Arkoh-Nelson <arkohnelsonemmanuel@gmail.com>
2024-02-08 20:08:44 +0000

    Implemented a while loop into the game
    This allows the player to continue playing the game if  he/she wants to continue.
    Player can press 'y' or 'Y' to continue and press any other key to
    quit.

--- rps.py
+++ rps.py
@@ -8,30 +8,42 @@ class RPS(Enum):
     SCISSORS = 3
 
 
-print("")
-playerchoice = input("Enter...\n1 for Rock,\n2 for Paper, or \n3 for Scissors:\n\n")
+playagain = True
 
-player = int(playerchoice)
+while playagain:
+    playerchoice = input("\nEnter...\n1 for Rock,\n2 for Paper, or \n3 for Scissors:\n\n")
 
-if player < 1 or player > 3:
-    sys.exit("You must enter 1, 2, or 3.")
+    player = int(playerchoice)
+
+    if player < 1 or player > 3:
+        sys.exit("You must enter 1, 2, or 3.")
+        
+    computerchoice = random.choice("123")
+
+    computer = int(computerchoice)
+
+    print("\nYou chose " + str(RPS(player)).replace('RPS.', '') + ".")
+    print("Python chose " + str(RPS(computer)).replace('RPS.', '') + ".\n")
+
+    if player == 1 and computer == 3:
+        print("🎉  You win!")
+    elif player == 2 and computer == 1:
+        print("🎉  You win!")
+    elif player == 3 and computer == 2:
+        print("🎉  You win!")
+    elif player == computer:
+        print("😲  Tie game!")
+    else:
+        print("🐍  Python wins!")
+        
+    playagain = input("\nPlay again? \nY for Yes or \nQ to Quit \n\n")
     
-computerchoice = random.choice("123")
-
-computer = int(computerchoice)
-
-print("")
-print("You chose " + str(RPS(player)).replace('RPS.', '') + ".")
-print("Python chose " + str(RPS(computer)).replace('RPS.', '') + ".")
-print("")
-
-if player == 1 and computer == 3:
-    print("🎉  You win!")
-elif player == 2 and computer == 1:
-    print("🎉  You win!")
-elif player == 3 and computer == 2:
-    print("🎉  You win!")
-elif player == computer:
-    print("😲  Tie game!")
-else:
-    print("🐍  Python wins!")
+    if playagain.lower() == "y":
+        continue
+    else:
+        print("\n🎉🎉🎉")
+        print("Thank you for playing!\n")
+        playagain = False
+        
+sys.exit("Bye! 👋")        
+        
